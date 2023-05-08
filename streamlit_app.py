4a596aebeb6250967fa8788b8b0e5110a83c4b03
jerryren <70980323+jerryren1712@users.noreply.github.com>
2023-05-08 16:02:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie for Evy! 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 
